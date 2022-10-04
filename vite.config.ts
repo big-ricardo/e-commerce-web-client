17ff59572c74821751d9b8974c509b12ea2c6c71
Luis Ricardo <luis.ricardo@tech4h.com.br>
2022-10-04 08:39:16 -0300

    feat: archives  for config

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    include: ["**/*(*.)?{test,spec}.{ts,tsx}"],
+    exclude: ["node_modules", "dist", ".idea", ".git", ".cache"],
+  },
+});
